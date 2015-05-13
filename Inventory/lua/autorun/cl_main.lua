065520d5ad9b3baa88987dc18bba1aed3cc5f4c9
AnarchyCoding <immranarchy@gmail.com>
2015-05-13 10:31:08 -0500

    Client side derma design is finished
    Todo:
    
    1. Add inventory refresh
    2. Add drag'n'drop functionality

--- Inventory/lua/autorun/cl_main.lua
+++ Inventory/lua/autorun/cl_main.lua
@@ -12,10 +12,10 @@ if (SERVER) then return end
 
 local function AddChat(text, sender)
 
-	if sender == 1 then
+	if sender == 1 then // sent from client
 		
 		chat.AddText(Color(145,220,135), "[Inventory] ", color_white, text)
-	else
+	else // must have been sent from server...
 		local text = net.ReadString()
 		
 		chat.AddText(Color(145,220,135), "[Inventory] ", color_white, text)
@@ -29,7 +29,9 @@ local set = {
 	boxX = 50,
 	boxY = 50,
 	boxesW = 10,
-	boxesT = 5
+	boxesT = 5,
+	title = "Inventory",
+	font = "DermaDefault"
 }
 
 local inventory = {}
@@ -38,22 +40,91 @@ local function ReceiveInv()
 end
 net.Receive("SendInv", ReceiveInv)
 
+// Make our nice blur effect
+
+local blur = Material("pp/blurscreen")
+
+local function DrawBlurPanel(panel, amount, heavyness)
+
+
+	local x, y = panel:LocalToScreen(0, 0)
+	local scrW, scrH = ScrW(), ScrH()
+
+	surface.SetDrawColor(255,255,255)
+	surface.SetMaterial(blur)
+
+	for i = 1, heavyness do
+		blur:SetFloat("$blur", (i / 3) * (amount or 6))
+		blur:Recompute()
+
+		render.UpdateScreenEffectTexture()
+		surface.DrawTexturedRect(x * -1, y * -1, scrW, scrH)
+	end
+end
+
+// Create an easy button
+
+
+local function DermaBut(text,method)
+
+	local but = vgui.Create("DButton")
+	but:SetText("")
+	local col_cold, col_hot = Color(0, 0, 0, 0), Color(255, 90, 90, 100)
+	local rect_col, text_col = color_white, color_white
+	but.Paint = function(self, w, h)
+		surface.SetDrawColor(self:IsHovered() and col_hot or col_cold)
+		surface.DrawRect(0, 0, w, h)
+			
+		surface.SetDrawColor(rect_col)
+		surface.DrawOutlinedRect(0, 0, w, h)
+
+		draw.SimpleTextOutlined(text, set.font, w/2, h/2, text_col, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, 1, color_black)
+	end
+	
+	but.DoClick = function()
+	
+		surface.PlaySound("UI/buttonclick.wav")
+		method()
+	end
+	
+	return but
+end
+
 local function Main()
 
 	local frame = vgui.Create("DFrame")
 	frame:SetSize(set.boxX*set.boxesW + set.pad*set.boxesW+set.pad, set.boxY*set.boxesT + set.pad*set.boxesW+set.pad)
 	frame:Center()
-	frame:SetTitle("Inv")
+	frame:SetTitle("")
 	frame:SetDraggable(false)
+	frame:ShowCloseButton(false)
 	frame:MakePopup()
+	frame.Paint = function(self,w,h)
+		DrawBlurPanel(self, 1, 15)
+		
+		surface.SetDrawColor(color_white)
+		surface.DrawOutlinedRect(0,0,w-34,25)
+		
+		draw.SimpleTextOutlined(set.title, set.font, 5, 5, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_LEFT, 1, color_black)
+	end
+	
+	local closeBut = DermaBut("X",function() 
+	
+		if frame:IsValid() && IsValid(frame) then
+			frame:Close()
+		end
+	end)
+	closeBut:SetParent(frame)
+	closeBut:SetPos(frame:GetWide()-35,0)
+	closeBut:SetSize(35,25)
 	
 	local iconLayout = vgui.Create("DIconLayout", frame)
 	iconLayout:SetSize(frame:GetWide()-10, frame:GetTall()-35)
 	iconLayout:SetPos(5,30)
 	iconLayout:SetSpaceY(5)
 	iconLayout:SetSpaceX(5)
-	iconLayout.Paint = function( self )
-		draw.RoundedBox(0, 0, 0, self:GetWide(), self:GetTall(), Color(55,55,55,0))
+	iconLayout.Paint = function(self,w,h)
+		draw.RoundedBox(0, 0, 0, w, h, Color(55,55,55,0))
 	end
 	
 	for k,v in pairs(inventory) do
@@ -61,6 +132,13 @@ local function Main()
 			local slot = vgui.Create("DPanel", iconLayout)
 			slot:SetSize(50, 50)
 			slot:SetBackgroundColor(Color(0, 0, 0, 130))
+			slot.Paint = function(self,w,h)
+				surface.SetDrawColor(slot:GetBackgroundColor())
+				surface.DrawRect(0,0,w,h)
+				
+				surface.SetDrawColor(color_white)
+				surface.DrawOutlinedRect(0,0,w,h)
+			end
 
 			if m.class ~= nil then // if there's a class
 			
@@ -70,6 +148,19 @@ local function Main()
 				item:SetCamPos(Vector(20, 20, 5))
 				item:SetLookAt(Vector(0, 0, 0))
 				item:SetTooltip(m.class)
+				
+				item.OnCursorEntered = function()
+					item:GetParent():SetBackgroundColor(Color(41, 128, 185, 100))
+				end
+				item.OnCursorExited = function()
+					item:GetParent():SetBackgroundColor(Color(0, 0, 0, 130))
+				end
+				item.PaintOver = function()
+					if item:IsHovered() then
+						surface.SetDrawColor(color_white)
+						surface.DrawOutlinedRect(0, 0, 50, 50)
+					end
+				end
 				
 				function item:OnMousePressed(but)
 					
